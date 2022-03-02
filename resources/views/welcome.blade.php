13ef1e8c1750f43e52f1864719114415a74d8478
Jairo Ushiña <jago86@gmail.com>
2022-03-02 00:43:10 -0500

    M1. Clase 6.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,21 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Blog de Jairo</title>
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
-    <article>
-        <h1>Curso de Laravel 9.</h1>
-        <p>Sit commodo est nostrud sit fugiat amet laboris mollit. Amet ipsum anim non id sit sit pariatur excepteur non fugiat. Eu ipsum id sit exercitation consectetur. Sint velit ullamco ea qui proident aliquip exercitation pariatur adipisicing sit Lorem cillum occaecat sit. Fugiat irure dolor commodo magna nulla et Lorem est.</p>
-    </article>
+    <div class="container">
+        <article>
+            <h1>Curso de Laravel 9.</h1>
+            <p>Sit commodo est nostrud sit fugiat amet laboris mollit. Amet ipsum anim non id sit sit pariatur excepteur non fugiat. Eu ipsum id sit exercitation consectetur. Sint velit ullamco ea qui proident aliquip exercitation pariatur adipisicing sit Lorem cillum occaecat sit. Fugiat irure dolor commodo magna nulla et Lorem est.</p>
+        </article>
 
-    <article>
-        <h1>Novedades de Laravel 9.</h1>
-        <p>Sit commodo est nostrud sit fugiat amet laboris mollit. Amet ipsum anim non id sit sit pariatur excepteur non fugiat. Eu ipsum id sit exercitation consectetur. Sint velit ullamco ea qui proident aliquip exercitation pariatur adipisicing sit Lorem cillum occaecat sit. Fugiat irure dolor commodo magna nulla et Lorem est.</p>
-    </article>
+        <article>
+            <h1>Novedades de Laravel 9.</h1>
+            <p>Sit commodo est nostrud sit fugiat amet laboris mollit. Amet ipsum anim non id sit sit pariatur excepteur non fugiat. Eu ipsum id sit exercitation consectetur. Sint velit ullamco ea qui proident aliquip exercitation pariatur adipisicing sit Lorem cillum occaecat sit. Fugiat irure dolor commodo magna nulla et Lorem est.</p>
+        </article>
 
-    <article>
-        <h1>Manejo básico de Eloquent.</h1>
-        <p>Sit commodo est nostrud sit fugiat amet laboris mollit. Amet ipsum anim non id sit sit pariatur excepteur non fugiat. Eu ipsum id sit exercitation consectetur. Sint velit ullamco ea qui proident aliquip exercitation pariatur adipisicing sit Lorem cillum occaecat sit. Fugiat irure dolor commodo magna nulla et Lorem est.</p>
-    </article>
+        <article>
+            <h1>Manejo básico de Eloquent.</h1>
+            <p>Sit commodo est nostrud sit fugiat amet laboris mollit. Amet ipsum anim non id sit sit pariatur excepteur non fugiat. Eu ipsum id sit exercitation consectetur. Sint velit ullamco ea qui proident aliquip exercitation pariatur adipisicing sit Lorem cillum occaecat sit. Fugiat irure dolor commodo magna nulla et Lorem est.</p>
+        </article>
+    </div>
 </body>
 </html>
