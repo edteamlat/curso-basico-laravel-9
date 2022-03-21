2efc28a16b7dfa00b7dc9a6f7bf7683a1da288c6
Jairo Ushiña <jago86@gmail.com>
2022-03-21 15:30:50 -0500

    M3. Clase 6.

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,6 @@
 <footer class="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-800">
     <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-    <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com" class="hover:underline">Blog de Jairo</a>. All Rights Reserved.
+    <x-latest-posts number="3" ></x-latest-posts>
+    <span class="mt-5 block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com" class="hover:underline">Blog de Jairo</a>. All Rights Reserved.
     </span>
 </footer>
