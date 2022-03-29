bbfd78681ecb43f8837e6cbaa7956a1e98cae229
Jairo Ushiña <jago86@gmail.com>
2022-03-28 23:31:37 -0500

    M4. Clase 8.

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -41,6 +41,7 @@ class RegisteredUserController extends Controller
 
         $user = User::create([
             'name' => $request->name,
+            'last_name' => $request->last_name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
